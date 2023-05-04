462b0436cdcef174d802685a2c6142cbd48e0770
EndocrineMSc <bamsc.studio@gmail.com>
2023-05-04 20:01:56 +0200

    Added readme and PauseControl Summary

---
+++ Assets/Readme/Readme.cs
@@ -0,0 +1,10 @@
+/*
+ * I hope the scripts in this library a somewhat self-explanatory.
+ * Except for maybe the player controller, but fiddle around with that a bit and I think
+ * you'll get it.
+ * 
+ * Some scripts use the free version of the DOTween library, which simplifies
+ * lerping sprites, images, animations and more. You can get it in the package manager.
+ * The paid version is NOT necessary. If you also do not want to use DOTween, you can
+ * just delete all script line that contain *.DO*.
+ */
--- Assets/Scripts/PauseControl.cs
+++ Assets/Scripts/PauseControl.cs
@@ -4,6 +4,13 @@ using UnityEngine.Events;
 
 namespace Utility
 {
+    /// <summary>
+    /// Let's you pause the game.
+    /// Freezes time and therefore all time-based movement and animations.
+    /// Will not freeze all input, so you can still press buttons and move the mouse.
+    /// If you have movement by inputs, that is not time-based, put a check for GameIsPaused 
+    /// in the movement script.
+    /// </summary>
     internal class PauseControl : MonoBehaviour
     {
         #region Fields and Properties
